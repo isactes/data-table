ee61014e8be6744d54453b3f57ff6603884f490f
isactes <fidel.renovato@gmail.com>
2022-05-19 21:19:37 -0500

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListViewsComponent } from './list-views/list-views.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListViewsComponent
   ],
   imports: [
     BrowserModule,
